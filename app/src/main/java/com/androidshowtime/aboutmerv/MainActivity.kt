f751f356b3f6dc8fe5d6b6f9a48e433b636dcab9
Antony <vontonnie@gmail.com>
2020-11-11 14:00:33 +0300

    Initial commit

---
+++ app/src/main/java/com/androidshowtime/aboutmerv/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androidshowtime.aboutmerv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
